37f4c680368dca45ec1ec3dbf289b26b04082be5
Tatjana Bezsazna <Tatjana Bezsazna>
2017-04-02 08:32:55 +0300

    Add automatic tests

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -7,7 +7,7 @@ var Nav = () => {
 				<div className="top-bar">
 					<div className="top-bar-left">
 						<ul className="menu">
-							<li className='menu-text'>React Time App</li>
+							<li className='menu-text'>React Timer App</li>
 							<li><Link to='/' activeClassName="active-link">Timer</Link></li>
 							<li><Link to='/countdown' activeClassName="active-link">Countdown</Link></li>
 						</ul>
